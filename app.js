fc6303f62c924848b1346bc8ccd2f6fb7500c8f2
Darya <dashachel74@yandex.ru>
2024-11-03 01:49:36 +0500

    Решение задачи  - Расчет проекта

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('hello');
+//Задача - расчета стоимости заказа и определение 
+//возможности его выполнить при ограниченном кол-ве времени
+
+
+//Решение(функции)
+function canculateCost(rateUSD, projectHours) {
+    return rateUSD * projectHours + '$'
+}
+
+function isPosible(daysUntilDeadline, projectHours, workingHoursPerDay) {
+    const nowDate = new Date();
+    const deadlineDate = new Date();
+    deadlineDate.setDate(deadlineDate.getDate() + daysUntilDeadline);
+
+    while(projectHours) {
+        if (nowDate.getDay !== 0 && nowDate.getDay !== 6) {
+            projectHours -= workingHoursPerDay;
+            nowDate.setDate(nowDate.getDate() + 1);
+        }
+    }
+    if (deadlineDate.valueOf() > nowDate.valueOf()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Данные 
+const rate = 80;
+const workingHoursPerDay = 5;
+const daysUntilDeadline = 11;
+const projectHours = 40;
+
+//Вывод
+console.log('Стоимость заказа' + canculateCost(rate, projectHours));
+console.log('Смогу ли взять заказ?' +  isPosible(daysUntilDeadline, projectHours, workingHoursPerDay));
